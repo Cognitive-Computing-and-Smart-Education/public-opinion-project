05841291657258da5682bc5f48f35025178212c3
syx9527 <1275241305@qq.com>
2021-08-12 16:05:27 +0800

    分布式

--- PeopleSpider/PeopleSpider/tt.py
+++ PeopleSpider/PeopleSpider/tt.py
@@ -5,9 +5,9 @@ keys = ['教育', '教学', '体育教育', '智慧教育', '科技', '体育', 
         '艺术培训', '远程教育', '线下教育', 'steam教育', '应试教育', '中考', '高考', '课外辅导', '科普教育', '海外教育', '爱国教育', ]
 # keys = ['K12']
 
-# for _ in keys:
-#     rd.lpush('pp:keyword', _)
-
-
 for _ in keys:
-    rd.lpush('xinhuanews:keyword', _)
+    rd.lpush('pp:keyword', _)
+
+#
+# for _ in keys:
+#     rd.lpush('xinhuanews:keyword', _)
